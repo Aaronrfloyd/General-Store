4e97496320441108bbe839f9ed2fd4678f99bde9
Aaron Floyd <aaronrfloyd@gmail.com>
2019-10-08 15:56:36 -0400

    Add project files.

---
+++ GeneralStore/Models/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.Models
+{
+    public class Customer
+    {
+        //ID
+        //FirstName
+        //LastName
+        //Contact Info
+        //IsPleasant
+        [Key]
+        public int CustomerID { get; set; }
+
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string Lastname { get; set; }
+
+
+        [Required]
+        [Display(Name = "Contact")]
+        public string ContactInfo { get; set; }
+
+
+        [Required]
+        [Display(Name = "Cool")]
+        public bool IsPlesant { get; set; }
+        
+        
+
+    }
+}
+
+    
+
+
